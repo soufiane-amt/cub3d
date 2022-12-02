0fe0b895a7bf73927a289cc262ad2f5205d1ae47
Soufiane Amajat <samajat@e1r6p15.1337.ma>
2022-12-02 15:10:25 +0100

    reimplementing the move_is_valid func

--- src/graphic/events/movesListener.c
+++ src/graphic/events/movesListener.c
@@ -6,7 +6,7 @@
 /*   By: samajat <samajat@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/24 19:02:04 by samajat           #+#    #+#             */
-/*   Updated: 2022/12/02 14:45:34 by samajat          ###   ########.fr       */
+/*   Updated: 2022/12/02 15:06:46 by samajat          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,36 +64,59 @@ int     move_is_valid (t_point  dstPoint)
 // }
 void    moveLeft(t_player  *player)
 {
-    // t_point dstPoint;
-    float   radian;
+    t_point dstPoint;
+    float   radianValue;
 
-    radian = convert_degree_to_radian(player->pos.direction + 90);
-    player->pos.origPoint.X -= (PLAYER_SPEED * cos(radian));
-    player->pos.origPoint.Y -= (PLAYER_SPEED * sin(radian));
+    radianValue = convert_degree_to_radian(player->pos.direction + 90);
+    dstPoint.X = player->pos.origPoint.X - (PLAYER_SPEED * cos(radianValue));
+    dstPoint.Y = player->pos.origPoint.Y - (PLAYER_SPEED * sin(radianValue));
+    if (move_is_valid(dstPoint))
+    {
+        player->pos.origPoint.X -= (PLAYER_SPEED * cos(radianValue));
+        player->pos.origPoint.Y -= (PLAYER_SPEED * sin(radianValue));
+    }
 }
 void    moveRight(t_player  *player)
 {
-    float   radian;
+    t_point dstPoint;
+    float   radianValue;
 
-    radian = convert_degree_to_radian(player->pos.direction + 90);
-    player->pos.origPoint.X += (PLAYER_SPEED * cos(radian));
-    player->pos.origPoint.Y += (PLAYER_SPEED * sin(radian));
+    radianValue = convert_degree_to_radian(player->pos.direction + 90);
+    dstPoint.X = player->pos.origPoint.X + (PLAYER_SPEED * cos(radianValue));
+    dstPoint.Y = player->pos.origPoint.Y + (PLAYER_SPEED * sin(radianValue));
+    if (move_is_valid((dstPoint)))
+    {
+        player->pos.origPoint.X += (PLAYER_SPEED * cos(radianValue));
+        player->pos.origPoint.Y += (PLAYER_SPEED * sin(radianValue));
+    }
 }
 
 void    moveBack(t_player  *player)
 {
-    float   radian;
+    t_point dstPoint;
+    float   radianValue;
 
-    radian = convert_degree_to_radian(player->pos.direction);
-    player->pos.origPoint.X -= (PLAYER_SPEED * cos(radian));
-    player->pos.origPoint.Y -= (PLAYER_SPEED * sin(radian));
+    radianValue = convert_degree_to_radian(player->pos.direction);
+    dstPoint.X = player->pos.origPoint.X - (PLAYER_SPEED * cos(radianValue));
+    dstPoint.Y = player->pos.origPoint.Y - (PLAYER_SPEED * sin(radianValue));
+    if (move_is_valid((dstPoint)))
+    {
+        player->pos.origPoint.X -= (PLAYER_SPEED * cos(radianValue));
+        player->pos.origPoint.Y -= (PLAYER_SPEED * sin(radianValue));
+    }
 }
 
 void    moveFront(t_player  *player)
 {
-    float   radian;
+    t_point dstPoint;
+    float   radianValue;
 
-    radian = convert_degree_to_radian(player->pos.direction);
-    player->pos.origPoint.X += (PLAYER_SPEED * cos(radian));
-    player->pos.origPoint.Y += (PLAYER_SPEED * sin(radian));
+    radianValue = convert_degree_to_radian(player->pos.direction);
+    dstPoint.X = player->pos.origPoint.X + (PLAYER_SPEED * cos(radianValue));
+    dstPoint.Y = player->pos.origPoint.Y + (PLAYER_SPEED * sin(radianValue));
+    if (move_is_valid((dstPoint)))
+    {
+        player->pos.origPoint.X += (PLAYER_SPEED * cos(radianValue));
+        player->pos.origPoint.Y += (PLAYER_SPEED * sin(radianValue));
+    }
 }
